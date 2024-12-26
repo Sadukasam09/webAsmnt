ffadb4a74351756e94f12a93ee100fe542043165
Sadukasam09 <KIC-DCSAI-241-F-039@student.nibm.lk>
2024-12-26 13:59:39 +0530

    my web files

---
+++ sadukaweb/php/codes.php
@@ -0,0 +1,45 @@
+
+
+
+<!--            Php -- hyper text prepeocessor
+                php <--- is server side language
+                        php is used to create dynamic web pages 
+
+
+    first php code
+<?php
+    echo "Hello World";
+?>
+
+<!-- how to run  php code
+    1. install xampp
+    2. start apache server
+    3. create a file with .php extension
+    4. write php code
+    5. save the file in htdocs folder
+    6. open browser and type localhost/filename.php
+    7. you will see the output of the php code
+
+how to run php file with xampp
+    1. open xampp control panel
+    2. start apache server
+    3. open browser
+    4. type localhost/filename.php
+    5. you will see the output of the php code    
+   -->
+
+ PHP VARIABLES
+
+         1.$variable_name = value;
+            --> $weight = 60;
+            --> $name = "saduka";
+            --> $age = 20;
+            --> $height = 5.6;
+
+            html tags we can use inside php with only echo statement
+                    --> echo "<h1>hello world</h1>";
+                    --> echo "<p>hello world</p>";
+
+                    design backend with php
+                    
+
